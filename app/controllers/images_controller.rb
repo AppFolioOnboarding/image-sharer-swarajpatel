7d874f8ff5d47ceab3d797dacc9e87287d490aa0
Swaraj Patel <swarajpatel37@gmail.com>
2019-07-03 10:59:04 -0700

    SP - Add create action for image controller

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,4 +2,13 @@ class ImagesController < ApplicationController
   def new
     @image = Image.new
   end
+
+  def create
+    @image = Image.new(params[:image].permit(:url))
+    if @image.valid?
+      @image.save!
+    else
+      render :new
+    end
+  end
 end
--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -7,4 +7,21 @@ class ImagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :ok
     assert_select '.btn', count: 1
   end
+
+  def test_create__success
+    assert_difference 'Image.count', 1 do
+      post images_path, params: {
+        image: { url: 'http://webapps-for-beginners.rubymonstas.org/assets/images/erb_1-b7c55b20.png' }
+      }
+    end
+  end
+
+  def test_create__failure
+    assert_no_difference 'Image.count' do
+      post images_path, params: { image: { url: 'invalid url' } }
+    end
+
+    assert_response :ok
+    assert_includes response.body, 'needs to be a valid image URL'
+  end
 end
