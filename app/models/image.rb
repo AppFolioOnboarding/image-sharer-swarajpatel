8e12a7847d30c7dee05e139e041cf70b364ab9f1
Swaraj Patel <swarajpatel37@gmail.com>
2019-07-05 15:12:33 -0700

    sp - Add tags to image model

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,4 +2,5 @@
 class Image < ApplicationRecord
   validates :url, presence: true, format: { with: %r{(http(s?):)([\/|.|\w|\s|-])*\.(?:jpg|gif|png)},
                                             message: 'needs to be a valid image URL' }
+  acts_as_taggable_on :tags
 end
