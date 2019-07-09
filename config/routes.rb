AppFolioOnboarding/image-sharer-eeszy

bs - add api endpoint

config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#home'
+
+  namespace :api do
+    resource :feedback, only: [:create]
+  end
 end

AppFolioOnboarding/image-sharer-swarajpatel

config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   root 'images#index'
 
   resources :feedbacks, only: [:new]
+
+  namespace :api do
+    resource :feedbacks, only: [:create]
+  end
 end