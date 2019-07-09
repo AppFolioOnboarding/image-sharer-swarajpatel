98a0d527a7006d95e1a95458aff468be6fe6d6ce
Swaraj Patel <swarajpatel37@gmail.com>
2019-07-09 13:25:40 -0700

    sp - Add test for app component

--- app/javascript/image_sharing/test/components/App.spec.js
+++ app/javascript/image_sharing/test/components/App.spec.js
@@ -9,5 +9,12 @@ describe('<App />', () => {
   it('should render correctly', () => {
     const wrapper = mount(<App />);
     assert(wrapper.contains('Tell us what you think'));
+
+    assert.strictEqual(wrapper.find('Header').length, 1);
+    assert.strictEqual(wrapper.find('Form').length, 1);
+    assert.strictEqual(wrapper.find('Footer').length, 1);
+
+    assert.strictEqual(wrapper.find('Header').prop('title'), 'Tell us what you think');
+    assert.strictEqual(wrapper.find('Footer').prop('text'), 'Copyright: Appfolio Inc. Onboarding');
   });
 });
